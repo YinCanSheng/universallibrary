bba5e9277805ef046d54e26c055cd30de5854996
Mo <chenmmren@qq.com>
2018-03-06 20:24:15 +0800

    初出版

---
+++ nbprolib/universallibrary/src/main/java/ch/dynashar/nbpro/universallibrary/NB/ControlBuildBus.java
@@ -0,0 +1,9 @@
+package ch.dynashar.nbpro.universallibrary.NB;
+
+/**
+ * Created by 今夜犬吠 on 2018/3/6.
+ * 调度构建-总线
+ */
+
+public class ControlBuildBus {
+}
---
+++ nbprolib/universallibrary/src/main/java/ch/dynashar/nbpro/universallibrary/NB/ProTotalControl.java
@@ -0,0 +1,9 @@
+package ch.dynashar.nbpro.universallibrary.NB;
+
+/**
+ * Created by 今夜犬吠 on 2018/3/6.
+ * 工程-阀门
+ */
+
+public class ProTotalControl {
+}
